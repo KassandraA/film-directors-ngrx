35c54a0ccc878b4998990060d681a92e69c48d04
Ksenia Ann <kseniagor.link@gmail.com>
2020-08-23 13:29:33 +0200

    application works

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,9 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual(
-      'film-directors-ngrx-v2 app is running!'
-    );
+    expect(page.getTitleText()).toEqual('film-directors-ngrx app is running!');
   });
 
   afterEach(async () => {
